dff2d8ee4ed829972735732c78fe9f394dd6c44f
Apart <carsonstary@gmail.com>
2022-11-15 13:00:43 -0500

    Add functionality to brain world

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,16 @@
+using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyHit : MonoBehaviour
+{
+    public string newLevel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Initiate.Fade(newLevel,Color.black,1);
+    }
+
+    void Start() {}
+}
